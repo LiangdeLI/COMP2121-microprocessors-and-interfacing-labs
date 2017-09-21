c2aaa9235d4b17deb5509fdbefa2cd2951aea585
DESKTOP-SHFKFBT\Lee <liliangde88@gmail.com>
2017-09-22 02:26:28 +1000

    TaskA working

--- led/TaskA.asm
+++ led/TaskA.asm
@@ -25,31 +25,32 @@ sbic PINF, 0 ; Skip the next instruction if PB0 is pushed
 rjmp switch1 ; If not pushed, check the other switch
 sub temp, r17
 out PORTC, temp
+ldi r18, 0x90   ;26000
+ldi r19, 0x65
+;ldi r20, 0x00
 delay1:
-ldi r18, 0x9A
-ldi r19, 0x99
-ldi r20, 0x19
 sub r18, r17
 sbc r19, r21
-sbc r20, r21
+;sbc r20, r21
 cp r18, r21
 cpc r19, r21
-cpc r20, r21
+;cpc r20, r21
 brne delay1
 switch1:
 sbic PINF, 1 ; Skip the next instruction if PB1 is pushed
 rjmp switch0 ; If not pushed, check the other switch
 inc temp
 out PORTC, temp
+ldi r18, 0x90
+ldi r19, 0x65
+;ldi r20, 0x00
 delay2:
-ldi r18, 0x9A
-ldi r19, 0x99
-ldi r20, 0x19
 sub r18, r17
 sbc r19, r21
-sbc r20, r21
+;sbc r20, r21
 cp r18, r21
 cpc r19, r21
-cpc r20, r21
+;cpc r20, r21
 brne delay2
 rjmp switch0 ; Now check PB0 again
+
